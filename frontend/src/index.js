789553f285a1f6b3f5b5c112a543cb3bc87f32f5
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2025-02-02 11:40:07 +0100

    finally, images good, and index.html is good

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import "./styles.css";
 import headerImage from "./assets/images/header.jpg";
 
 document.body.style.backgroundImage = `url(${headerImage})`;
-document.body.style.backgroundSize = "cover";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
