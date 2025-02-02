6e1fbe44882360623a30651947ccafb206d7cfbc
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2025-02-02 11:28:33 +0100

    test, not working, other way

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import headerImage from "./assets/images/header.jpg";
 
 document.body.style.backgroundImage = `url(${headerImage})`;
+document.body.style.backgroundSize = "cover";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
